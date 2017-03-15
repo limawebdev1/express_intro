bd21f2b9a2b8d86bce40fa777a63f3af48305bfb
Lisa Ma <limawebdev@gmail.com>
2017-03-15 09:55:25 -0600

    pretty much good

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+var usersPath = path.join(__dirname, '../users.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,6 +12,15 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next){
   var name = req.body.name;
   //figure out what to do with the request data
+  //check database if user exists
+  //new user -> send a welcome message and show them the leaderboard and current points
+  fs.readFile(usersPath, 'utf8', (err, userData) => {
+    if(err){
+      console.log(err);
+    }else {
+      console.log(userData);
+    }
+  })
   res.end();
 });
 
